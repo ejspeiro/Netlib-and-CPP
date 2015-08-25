862dc21bf7ed133899872239cb4e2a9b9168f814
ejspeiro <ejspeiro@gmail.com>
2015-08-24 17:46:00 -0700

    Fixed test case number two for dgemm_test.cc.
    - dgemm_test.cc: Fixed test case number 2.

--- dgemm_test.cc
+++ dgemm_test.cc
@@ -14,8 +14,8 @@ C := alpha*op(a)*op(b) + beta*c
 
 \sa http://www.math.utah.edu/software/lapack/lapack-blas/dgemm.html
 
-\param[in]      transa  Is this the transpose of the matrix a?
-\param[in]      transb  Is this the transpose of the matrix b?
+\param[in]      transa  Is a in row-major storage?
+\param[in]      transb  Is b in row-major storage?
 \param[in]      m       The number of rows of the matrices a and c.  m >= 0.
 \param[in]      n       The number of cols of the matrices b and c.  n >= 0.
 \param[in]      k       The number of cols of a and rows of c.  k >= 0.
@@ -28,26 +28,26 @@ C := alpha*op(a)*op(b) + beta*c
 \param[in,out]  c       Matrix c.
 \param[in]      ldc     The leading dimension of c.
 */
-extern "C" void dgemm_(char *ta,
-                       char* tb,
+extern "C" void dgemm_(char *ta,      // 1.
+                       char *tb,
                        int *m,
                        int *n,
                        int *k,
                        double *alpha,
                        double *a,
-                       int *lda,
+                       int *lda,      // 8.
                        double *b,
                        int *ldb,
                        double *beta,
                        double *c,
-                       int *ldc);
+                       int *ldc);     // 13.
 
 int main () {
 
   std::cout << "Testing BLAS' dgemm_ in C++11." << std::endl;
   std::cout << std::endl;
 
-  // Explore the impact of row-major ordering and the usage of the C++11 STL,
+  // Explore the impact of row-major storage and the usage of the C++11 STL,
   // when using BLAS's dgemm_ routine.
 
   int mm{9};  // Rows of aa and rows of cc.
@@ -65,14 +65,14 @@ int main () {
 
   // First test case: matrices with column-major ordering.
 
-  char ta{'N'}; // Is aa's data transposed, i.e. is it in row-major ordering?
-  char tb{'N'}; // Is bb's data transposed, i.e. is it in row-major ordering?
+  char ta{'N'}; // Is aa's data in row-major storage?
+  char tb{'N'}; // Is bb's data in row-major storage?
 
-  // Data array of the aa matrix, in column-major ordering.
-  std::vector<double> aa_col_maj_ord{-0.0263766, 0.150229, -0.313059, 0.200632, 0.281068, -0.650341, 0.537914, -0.214473, 0.0344071, 0.0233982, -0.156083, 0.437434, -0.657155, 0.549301, -0.221726, 0.00200615, 0.0305288, -0.00770386};
+  // Data array of the aa matrix, in column-major storage.
+  std::vector<double> aa_col_maj_sto{-0.0263766, 0.150229, -0.313059, 0.200632, 0.281068, -0.650341, 0.537914, -0.214473, 0.0344071, 0.0233982, -0.156083, 0.437434, -0.657155, 0.549301, -0.221726, 0.00200615, 0.0305288, -0.00770386};
 
-  // Data array of bb matrix, in column-major ordering.
-  std::vector<double> bb_col_maj_ord{-12.8, -57.1678, -50.724, -356.35};
+  // Data array of bb matrix, in column-major storage.
+  std::vector<double> bb_col_maj_sto{-12.8, -57.1678, -50.724, -356.35};
 
   // These values correspond to the column-major versions of the matrices.
 
@@ -80,13 +80,13 @@ int main () {
   int ldb{std::max(1,kk)};  // Leading dimension of the bb matrix.
   int ldc{std::max(1,mm)};  // Leading dimension of the cc matrix.
 
-  std::cout << "aa_col_maj_ord =" << std::endl;
+  std::cout << "aa_col_maj_sto =" << std::endl;
   if (ta == 'N') {
     std::swap(aa_num_rows,aa_num_cols);
   }
   for (int ii = 0; ii < aa_num_rows; ++ii) {
     for (int jj = 0; jj < aa_num_cols; ++jj) {
-      std::cout << std::setw(12) << aa_col_maj_ord[ii*aa_num_cols + jj];
+      std::cout << std::setw(12) << aa_col_maj_sto[ii*aa_num_cols + jj];
     }
     std::cout << std::endl;
   }
@@ -95,13 +95,13 @@ int main () {
     std::swap(aa_num_rows,aa_num_cols);
   }
 
-  std::cout << "bb_col_maj_ord =" << std::endl;
+  std::cout << "bb_col_maj_sto =" << std::endl;
   if (tb == 'N') {
     std::swap(bb_num_rows,bb_num_cols);
   }
   for (int ii = 0; ii < bb_num_rows; ++ii) {
     for (int jj = 0; jj < bb_num_cols; ++jj) {
-      std::cout << std::setw(12) << bb_col_maj_ord[ii*bb_num_cols + jj];
+      std::cout << std::setw(12) << bb_col_maj_sto[ii*bb_num_cols + jj];
     }
     std::cout << std::endl;
   }
@@ -115,19 +115,19 @@ int main () {
   double alpha{1.0};  // First scalar coefficient.
   double beta{0.0};   // Second scalar coefficient.
 
-  std::vector<double> cc_col_maj_ord(mm*kk);  // Output matrix.
+  std::vector<double> cc_col_maj_sto(cc_num_rows*cc_num_cols);  // Output matrix.
 
-  dgemm_(&ta, &tb, &mm, &nn, &kk, &alpha, aa_col_maj_ord.data(), &lda,
-         bb_col_maj_ord.data(), &ldb, &beta, cc_col_maj_ord.data(), &ldc);
+  dgemm_(&ta, &tb, &mm, &nn, &kk, &alpha, aa_col_maj_sto.data(), &lda,
+         bb_col_maj_sto.data(), &ldb, &beta, cc_col_maj_sto.data(), &ldc);
 
   std::cout << "Test 1:" << std::endl;
   std::cout << std::endl;
 
-  std::cout << "cc_col_maj_ord =" << std::endl;
+  std::cout << "cc_col_maj_sto =" << std::endl;
   std::swap(cc_num_rows,cc_num_cols);
   for (int ii = 0; ii < cc_num_rows; ++ii) {
     for (int jj = 0; jj < cc_num_cols; ++jj) {
-      double vv = cc_col_maj_ord[ii*cc_num_cols + jj];
+      double vv = cc_col_maj_sto[ii*cc_num_cols + jj];
       std::cout << std::setw(12) << vv;
     }
     std::cout << std::endl;
@@ -135,14 +135,14 @@ int main () {
   std::cout << std::endl;
   std::swap(cc_num_rows,cc_num_cols);
 
-  // Output matrix will be given in column-major ordering.
+  // Output matrix will always be given in column-major storage.
 
-  // Second test case: What if we have a row-major ordered input matrix?
+  // Second test case: What if we have a row-major storage input matrix?
 
-  ta = 'T'; // State that now, the input WILL be in row-wise ordering.
-  tb = 'T'; // State that now, the input WILL be in row-wise ordering.
+  ta = 'T'; // State that now, the input will be in row-major storage.
+  tb = 'T'; // State that now, the input will be in row-major storage.
 
-  std::vector<double> aa_row_maj_ord = {
+  std::vector<double> aa_row_maj_sto = {
     -0.0263766, 0.0233982,
      0.150229, -0.156083,
     -0.313059,  0.437434,
@@ -153,7 +153,7 @@ int main () {
     -0.214473,  0.0305288,
      0.0344071,-0.00770386};  // Data array of aa matrix, in row-major ordering.
 
-  std::vector<double> bb_row_maj_ord = {
+  std::vector<double> bb_row_maj_sto = {
     -12.8,     -50.724,
     -57.1678, -356.35}; // Data array of bb matrix, in row-major ordering.
 
@@ -164,17 +164,17 @@ int main () {
   ldc = std::max(1,mm);
 
 
-  dgemm_(&ta, &tb, &mm, &nn, &kk, &alpha, aa_row_maj_ord.data(), &lda,
-         bb_col_maj_ord.data(), &ldb, &beta, cc_col_maj_ord.data(), &ldc);
+  dgemm_(&ta, &tb, &mm, &nn, &kk, &alpha, aa_row_maj_sto.data(), &lda,
+         bb_row_maj_sto.data(), &ldb, &beta, cc_col_maj_sto.data(), &ldc);
 
   std::cout << "Test 2:" << std::endl;
   std::cout << std::endl;
 
-  std::cout << "cc_col_maj_ord =" << std::endl;
+  std::cout << "cc_col_maj_sto =" << std::endl;
   std::swap(cc_num_rows,cc_num_cols);
   for (int ii = 0; ii < cc_num_rows; ++ii) {
     for (int jj = 0; jj < cc_num_cols; ++jj) {
-      std::cout << std::setw(12) << cc_col_maj_ord[ii*cc_num_cols + jj];
+      std::cout << std::setw(12) << cc_col_maj_sto[ii*cc_num_cols + jj];
     }
     std::cout << std::endl;
   }
